6b87fcf8dc2edc304dd8fe479d63b7d2d801b216
Richard Cummings <richardcummings@Richards-MacBook-Pro.local>
2018-04-16 13:07:00 -0400

    IBActions and data models

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Richard Cummings on 2018-04-16.
+//  Copyright © 2018 Richard Cummings. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}

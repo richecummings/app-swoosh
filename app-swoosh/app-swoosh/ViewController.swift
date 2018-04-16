f7d89ca271154d3a57c902efe7be92fbc5dfe126
Richard Cummings <richardcummings@Richards-MacBook-Pro.local>
2018-04-16 11:58:18 -0400

    Segues and unwind

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -19,12 +19,10 @@ class ViewController: UIViewController {
         swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
         bgImg.frame = view.frame
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
     }
-
 
 }
 

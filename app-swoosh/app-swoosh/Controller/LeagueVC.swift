694c8bbe2d238f158d34bf3adc1343d112649f14
Richard Cummings <richardcummings@Richards-MacBook-Pro.local>
2018-04-16 12:48:43 -0400

    Segues

---
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,36 @@
+//
+//  LeagueVC.swift
+//  app-swoosh
+//
+//  Created by Richard Cummings on 2018-04-16.
+//  Copyright © 2018 Richard Cummings. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
